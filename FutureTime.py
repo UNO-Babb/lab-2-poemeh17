296b530e3b2d5b2edbbaba3b658f55218e79cca7
poemeh17 <poem3h@gmail.com>
2025-09-07 19:02:02 +0000

    Lab 2 completed

--- FutureTime.py
+++ FutureTime.py
@@ -9,7 +9,7 @@ import datetime
 def main():
   #getting current time from system, storing to variable
   now = datetime.datetime.now()
-  currentHour = now.hour
+  currentHour = (now.hour - 5) % 24
   currentMinute = now.minute
 
   print (currentHour, currentMinute) #this is just for checking, we should delete it later
@@ -17,7 +17,15 @@ def main():
   #TODO:
   #Ask user for hours
   #Ask user for minutes
+  moreMins = 2
+  futureMins = (currentMinute + moreMins) % 60
+  extraHour = (currentMinute + moreMins) // 60
 
+  futureHour = (currentHour + extraHour) % 24
+
+  
+  print(futureHour)
+  print(futureMins)
   #Calculate the time after the user-supplied time has passed.
 
   #Do not use any if statements in calculating the time.
--- Magic8Ball.py
+++ Magic8Ball.py
@@ -10,9 +10,17 @@ def main():
   #Create a list of your responses.
   print("Magic 8 Ball")
   #Prompt the user for their question.
-
+  answers = ["absolutely not", "sure", "maybe", "ask again next time", "you already know the answer", "You got it, easy", "Its all in your court", "no", "yes"]
   #Answer question randomly with one of the options from your earlier list.
-
+  length = len(answers)
+  r = random.random() * length
+  
+  r = int(r) #cut off any decimal values
+  
+  print(r)
+  response = answers[r]
+  print(response)
+ 
 
 if __name__ == '__main__':
   main()
